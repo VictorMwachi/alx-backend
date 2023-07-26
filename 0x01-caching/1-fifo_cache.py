e93c3b120f110c85a0c03dd78e820d6a0cb184f5
Victor Mwachi <mudakizevictor@gmail.com>
2023-07-26 21:02:03 +0000

    1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""1. FIFO caching """
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """first item to be inserted will be the first to be deleted
+    """
+    def ___init__(self):
+        """initialize"""
+        super().__init__()
+        self.cache_data = OderedDict()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value
+        for the key key.If key or item is None, this method should
+        not do anything."""
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, item = self.cache_data.popitem(last=False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """
+        returns the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data
+        return None"""
+        return self.cache_data.get(key, None)
