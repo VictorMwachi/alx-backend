9ea24274a52e8bcb647fa172b61ae16ddb5047cf
victormwachi <mudakizevictor@gmail.com>
2023-07-27 00:32:26 +0300

    2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""1. FIFO caching """
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """last item to be inserted will be the first to be deleted
+    """
+    def ___init__(self):
+        """initialize"""
+        super().__init__()
+        self.cache_data = OderedDict()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value
+        for the key key.If key or item is None, this method should
+        not do anything."""
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key, item = self.cache_data.popitem()
+            print("DISCARD:", last_key)
+
+    def get(self, key):
+        """
+        returns the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data
+        return None"""
+        return self.cache_data.get(key, None)
