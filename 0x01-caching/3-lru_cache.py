1f8e081d2b410d80bde7d6f7a5addf36854c7382
victormwachi <mudakizevictor@gmail.com>
2023-07-27 01:04:38 +0300

    3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""3. LRU Caching """
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """last item to be inserted will be the first to be deleted
+    """
+    def ___init__(self):
+        """initialize"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value
+        for the key key.If key or item is None, this method should
+        not do anything."""
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key, item = self.cache_data.popitem(last=False)
+            print("DISCARD:", last_key)
+
+    def get(self, key):
+        """
+        returns the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data
+        return None"""
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        return self.cache_data.get(key, None)
