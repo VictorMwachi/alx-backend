b39fd306467139213a77bae664a1c87800dd6fed
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-07-27 00:08:06 +0300

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -19,7 +19,7 @@ class FIFOCache(BaseCaching):
         if key and item:
             self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, item = self.cache_data.popitem(last=False)
+            first_key, item = self.cache_data.popitem(False)
             print("DISCARD:", first_key)
 
     def get(self, key):
